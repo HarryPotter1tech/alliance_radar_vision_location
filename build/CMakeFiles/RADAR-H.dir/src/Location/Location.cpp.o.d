3df96ee58dd42dde1459c10a7fa0f9efcf48a0cf
huangziang <huangziang_2024@qq.com>
2026-04-06 14:31:18 +0800

    Ignore and untrack build artifacts

--- build/CMakeFiles/RADAR-H.dir/src/Location/Location.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/RADAR-H.dir/src/Location/Location.cpp.o: \
- /home/harryh/RADAR-2026/RADAR-LOCATION-CAMERA/src/Location/Location.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/RADAR-H.dir/src/Utility/BVH_tree/BVH_tree.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/RADAR-H.dir/src/Utility/BVH_tree/BVH_tree.cpp.o: \
- /home/harryh/RADAR-2026/RADAR-LOCATION-CAMERA/src/Utility/BVH\ tree/BVH\ tree.cpp \
- /usr/include/stdc-predef.h
